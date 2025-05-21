f2f23a217816addf8fa10ceb41ce3b1869eab3d8
Khadim Fall <khadimfall0503@gmail.com>
2025-05-21 15:27:14 +0000

    Application pour Agence Voyages

--- src/components/Vol.tsx
+++ src/components/Vol.tsx
@@ -2,11 +2,12 @@ import "../styles/style.css";
 
 function Vol() {
   return (
-    <div className="background-vol d-flex flex-column justify-content-center align-items-center text-center p-5">
+    <div className="background-vol d-flex flex-column justify-content-center align-items-center text-center">
       <h1 className="text-white fw-bold mb-3">Bienvenue à FlightDeck Airport</h1>
-      <p className="text-white fs-5 w-75">
-      FlightDeck Airport, votre passerelle vers le monde.
+      <p style={{ color: '#f0f0f0' }} className="fs-5 w-75">
+    FlightDeck Airport, votre passerelle vers le monde.
       </p>
+
     </div>
   );
 }
