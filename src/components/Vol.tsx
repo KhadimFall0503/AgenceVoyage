d7ebf9bdf124a5a6ab7538f7759a1f2f1a355bb2
Khadim Fall <khadimfall0503@gmail.com>
2025-06-11 17:55:33 +0000

    Modification taille image du fond

--- src/components/Vol.tsx
+++ src/components/Vol.tsx
@@ -3,11 +3,12 @@ import "../styles/style.css";
 function Vol() {
   return (
     <div className="background-vol d-flex flex-column justify-content-center align-items-center text-center">
-      <h1 className="text-white fw-bold mb-3">Bienvenue à FlightDeck Airport</h1>
-      <p style={{ color: '#f0f0f0' }} className="fs-5 w-75">
-    FlightDeck Airport, votre passerelle vers le monde.
+      <h1 className="text-white fw-bold mb-3">
+        Bienvenue à FlightDeck Airport
+      </h1>
+      <p className="fs-5 w-75" style={{ color: "#f0f0f0" }}>
+        Découvrez des destinations incroyables et profitez des meilleures offres de vols.
       </p>
-
     </div>
   );
 }
